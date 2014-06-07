6a826f0769e0d3c70a79b3ddcb3c0e478fcd1387
Johan Kjölhede <kjolhede@telia.com>
2014-06-07 02:13:27 +0200

    removed generated src from checked in resources

--- mgen-visualdesigner/src_generated/main/java/se/culvertsoft/mgen/visualdesigner/MGenClassRegistry.java
+++
@@ -1,8 +0,0 @@
-package se.culvertsoft.mgen.visualdesigner;
-
-public class MGenClassRegistry extends se.culvertsoft.mgen.javapack.classes.ClassRegistry {
-
-    public MGenClassRegistry() {
-        add(new se.culvertsoft.mgen.visualdesigner.model.MGenModuleClassRegistry());
-    }
-}
