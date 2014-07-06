529aabd87d3c039e797cc8a911068b0f46f36bf2
PhroZen <berglund86@gmail.com>
2014-07-06 21:55:58 +0200

    Testsuite for js: javascript -> java from rhino.

--- mgen-javascriptlib/src/test/javascript/specs/test.spec.js
+++ mgen-javascriptlib/src/test/javascript/specs/test.spec.js
@@ -1,22 +1,8 @@
-describe("A suite is just a function", function() {
-  var a;
-
-  it("and so is a spec", function() {
-    a = true;
-
-    expect(a).toBe(true);
-  });
-});
-
-
 describe("The 'toBe' matcher compares with ===", function() {
 
-  it("and has a positive case", function() {
-    expect(false).toBe(true);
+  it("JAVA IN JS", function() {
+    java.lang.System.out.println("HELLO")
   });
 
-  it("and can have a negative case", function() {
-    expect(false).not.toBe(true);
-  });
 });
 
