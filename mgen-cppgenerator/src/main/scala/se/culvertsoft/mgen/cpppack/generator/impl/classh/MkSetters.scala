7039862181f0dfd2d2084e9450d2f77b7be08204
Johan Kjölhede <kjolhede@telia.com>
2014-07-07 14:11:03 +0100

    Added c++ setters for subtypes to supertype fields so field setting can be chained

--- mgen-cppgenerator/src/main/scala/se/culvertsoft/mgen/cpppack/generator/impl/classh/MkSetters.scala
+++ mgen-cppgenerator/src/main/scala/se/culvertsoft/mgen/cpppack/generator/impl/classh/MkSetters.scala
@@ -4,7 +4,10 @@ import scala.collection.JavaConversions.asScalaBuffer
 
 import se.culvertsoft.mgen.api.model.CustomType
 import se.culvertsoft.mgen.api.model.Module
+import se.culvertsoft.mgen.compiler.internal.BuiltInGeneratorUtil.endl
+import se.culvertsoft.mgen.compiler.internal.BuiltInGeneratorUtil.ln
 import se.culvertsoft.mgen.compiler.util.SuperStringBuffer
+import se.culvertsoft.mgen.cpppack.generator.CppGenUtils
 import se.culvertsoft.mgen.cpppack.generator.CppTypeNames.getTypeName
 import se.culvertsoft.mgen.cpppack.generator.impl.Alias.set
 
@@ -16,31 +19,33 @@ object MkSetters {
 
     implicit val currentModule = module
 
-    for (field <- t.fields()) {
+    val thisFields = t.fields().toSeq
+    val superFields = t.getAllFieldsInclSuper() -- thisFields
 
-      // By-value-setters with 'const Polymorphic<T>&' in
-      txtBuffer
-        .tabs(1)
-        .textln(s"${t.shortName()}& ${set(field, s"const ${getTypeName(field)}& ${field.name()}")};")
+    val superString = CppGenUtils.getSuperTypeString(t)
 
-      // By-value-setters with 'const T&' in
-      if (field.typ().isMGenCreatedType() && field.isPolymorphic()) {
-        txtBuffer
-          .tabs(1)
-          .textln(s"${t.shortName()}& ${set(field, s"const ${getTypeName(field.typ(), false)} & ${field.name()}")};")
+    for (coll <- List(superFields, thisFields)) {
+
+      for (field <- coll) {
+
+        // By-value-setters with 'const Polymorphic<T>&' in
+        ln(1, s"${t.shortName()}& ${set(field, s"const ${getTypeName(field)}& ${field.name()}")};")
+
+        // By-value-setters with 'const T&' in
+        if (field.typ().isMGenCreatedType() && field.isPolymorphic()) {
+          ln(1, s"${t.shortName()}& ${set(field, s"const ${getTypeName(field.typ(), false)} & ${field.name()}")};")
+        }
+
+        // By-reference-setters with 'T *' in
+        if (field.typ().isMGenCreatedType() && field.isPolymorphic()) {
+          ln(1, s"${t.shortName()}& ${set(field, s"${getTypeName(field.typ(), false)} * ${field.name()}, const bool managePtr = true")};")
+        }
+
       }
-
-      // By-reference-setters with 'T *' in
-      if (field.typ().isMGenCreatedType() && field.isPolymorphic()) {
-        txtBuffer
-          .tabs(1)
-          .textln(s"${t.shortName()}& ${set(field, s"${getTypeName(field.typ(), false)} * ${field.name()}, const bool managePtr = true")};")
-      }
-
     }
 
     if (t.fields().nonEmpty)
-      txtBuffer.endl()
+      endl()
 
   }
 
