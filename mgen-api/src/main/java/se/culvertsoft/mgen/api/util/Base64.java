4b71b7199a3ca798b2d5430d06b658fbfc7c5d23
Johan Kjölhede <kjolhede@telia.com>
2014-07-07 16:36:35 +0200

    Added MigBase64 converter (Removed dependency on suns/oracles base64 encoder/decoder).

--- mgen-api/src/main/java/se/culvertsoft/mgen/api/util/Base64.java
+++ mgen-api/src/main/java/se/culvertsoft/mgen/api/util/Base64.java
@@ -2,7 +2,7 @@ package se.culvertsoft.mgen.api.util;
 
 import java.io.IOException;
 
-import se.culvertsoft.mgen.api.util.internal.Base64Impl_TEMP;
+import se.culvertsoft.mgen.api.util.internal.Base64Impl_MIG;
 
 public class Base64 {
 
@@ -14,12 +14,20 @@ public class Base64 {
 		return encode(Bytes.encBigE(i)).substring(0, 3);
 	}
 
-	public static String encode(final byte[] bytes) {
-		return Base64Impl_TEMP.byteArrayToBase64(bytes);
+	public static short decodeShort(final String text) throws IOException {
+		return Bytes.decBigEShort(decode(text));
 	}
 
-	public static String encodeAlternate(final byte[] bytes) {
-		return Base64Impl_TEMP.byteArrayToAltBase64(bytes);
+	public static int decodeInt(final String text) throws IOException {
+		return Bytes.decBigEInt(decode(text));
+	}
+
+	public static String encode(final byte[] bytes) {
+		return Base64Impl_MIG.encodeToString(bytes, false);
+	}
+
+	public static byte[] decode(String text) {
+		return Base64Impl_MIG.decodeFast(ensureMultipleOf4(text));
 	}
 
 	public static String ensureMultipleOf4(String text) {
@@ -33,22 +41,6 @@ public class Base64 {
 		default:
 			return text;
 		}
-	}
-
-	public static short decodeShort(final String text) throws IOException {
-		return Bytes.decBigEShort(decode(text));
-	}
-
-	public static int decodeInt(final String text) throws IOException {
-		return Bytes.decBigEInt(decode(text));
-	}
-
-	public static byte[] decode(String text) {
-		return Base64Impl_TEMP.base64ToByteArray(ensureMultipleOf4(text));
-	}
-
-	public static byte[] decodeAlternate(final String text) {
-		return Base64Impl_TEMP.altBase64ToByteArray(ensureMultipleOf4(text));
 	}
 
 }
