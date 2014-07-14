d1d297827d6e99e71d8c56e0848328eba702c9d8
Johan Kjölhede <kjolhede@telia.com>
2014-07-14 23:51:06 +0100

    Improved C++ construction. Removed broken java caches for compiler types

--- mgen-cppgenerator/src/main/scala/se/culvertsoft/mgen/cpppack/generator/CppConstruction.scala
+++ mgen-cppgenerator/src/main/scala/se/culvertsoft/mgen/cpppack/generator/CppConstruction.scala
@@ -11,31 +11,26 @@ import se.culvertsoft.mgen.api.model.TypeEnum
 
 object CppConstruction {
 
-  val defCtroCache = new HashMap[(Type, Boolean), String]
-  val defNullCtroCache = new HashMap[(Type, Boolean), String]
-
   def defaultConstruct(
     typ: Type,
     isPolymorphicField: Boolean)(implicit currentModule: Module): String = {
 
-    defCtroCache.getOrElseUpdate((typ, isPolymorphicField), {
-      typ.typeEnum() match {
-        case TypeEnum.BOOL => "false"
-        case TypeEnum.INT8 => "(char)0"
-        case TypeEnum.INT16 => "(short)0"
-        case TypeEnum.INT32 => "0"
-        case TypeEnum.INT64 => "0LL"
-        case TypeEnum.FLOAT32 => "0.0f"
-        case TypeEnum.FLOAT64 => "0.0"
-        case TypeEnum.STRING => "\"\""
-        case TypeEnum.MAP => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.LIST => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.ARRAY => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.CUSTOM => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.ENUM => s"${typ.shortName()}_UNKNOWN"
-        case x => throw new GenerationException(s"Don't know how to handle type $x")
-      }
-    })
+    typ.typeEnum() match {
+      case TypeEnum.BOOL => "false"
+      case TypeEnum.INT8 => "(char)0"
+      case TypeEnum.INT16 => "(short)0"
+      case TypeEnum.INT32 => "0"
+      case TypeEnum.INT64 => "0LL"
+      case TypeEnum.FLOAT32 => "0.0f"
+      case TypeEnum.FLOAT64 => "0.0"
+      case TypeEnum.STRING => "\"\""
+      case TypeEnum.MAP => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.LIST => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.ARRAY => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.CUSTOM => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.ENUM => s"${typ.shortName()}_UNKNOWN"
+      case x => throw new GenerationException(s"Don't know how to handle type $x")
+    }
 
   }
 
@@ -43,16 +38,14 @@ object CppConstruction {
     typ: Type,
     isPolymorphicField: Boolean)(implicit currentModule: Module): String = {
 
-    defNullCtroCache.getOrElseUpdate((typ, isPolymorphicField), {
-      typ.typeEnum() match {
-        case TypeEnum.MAP => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.LIST => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.ARRAY => s"${getTypeName(typ, isPolymorphicField)}()"
-        case TypeEnum.CUSTOM => if (isPolymorphicField) "0" else defaultConstruct(typ, false)
-        case TypeEnum.ENUM => s"${typ.shortName()}_UNKNOWN"
-        case _ => s"${defaultConstruct(typ, isPolymorphicField)}"
-      }
-    })
+    typ.typeEnum() match {
+      case TypeEnum.MAP => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.LIST => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.ARRAY => s"${getTypeName(typ, isPolymorphicField)}()"
+      case TypeEnum.CUSTOM => if (isPolymorphicField) "0" else defaultConstruct(typ, false)
+      case TypeEnum.ENUM => s"${typ.shortName()}_UNKNOWN"
+      case _ => s"${defaultConstruct(typ, isPolymorphicField)}"
+    }
 
   }
 
--- mgen-javagenerator/src/main/scala/se/culvertsoft/mgen/javapack/generator/JavaConstruction.scala
+++ mgen-javagenerator/src/main/scala/se/culvertsoft/mgen/javapack/generator/JavaConstruction.scala
@@ -11,33 +11,28 @@ import se.culvertsoft.mgen.api.model.Field
 
 object JavaConstruction {
 
-  val defCtroCache = new HashMap[(Type, Boolean), String]
-  val defNullCtroCache = new HashMap[(Type, Boolean), String]
-
   def defaultConstruct(
     typ: Type,
     isGenericArg: Boolean = false)(implicit currentModule: Module): String = {
 
-    defCtroCache.getOrElseUpdate((typ, isGenericArg), {
-      typ.typeEnum() match {
-        case TypeEnum.BOOL => "false"
-        case TypeEnum.INT8 => "(byte)0"
-        case TypeEnum.INT16 => "(short)0"
-        case TypeEnum.INT32 => "0"
-        case TypeEnum.INT64 => "0L"
-        case TypeEnum.FLOAT32 => "0.0f"
-        case TypeEnum.FLOAT64 => "0.0"
-        case TypeEnum.STRING => "\"\""
-        case TypeEnum.MAP => s"new ${getTypeName(typ, isGenericArg)}()"
-        case TypeEnum.LIST => s"new ${getTypeName(typ, isGenericArg)}()"
-        case TypeEnum.ARRAY =>
-          val t = typ.asInstanceOf[ArrayType]
-          s"new ${getTypeName(t.elementType(), false, true)}[0]"          
-        case TypeEnum.ENUM => s"${getTypeName(typ)}.UNKNOWN"
-        case TypeEnum.CUSTOM => s"new ${getTypeName(typ, isGenericArg)}()"
-        case x => throw new GenerationException(s"Don't know how to handle type $x")
-      }
-    })
+    typ.typeEnum() match {
+      case TypeEnum.BOOL => "false"
+      case TypeEnum.INT8 => "(byte)0"
+      case TypeEnum.INT16 => "(short)0"
+      case TypeEnum.INT32 => "0"
+      case TypeEnum.INT64 => "0L"
+      case TypeEnum.FLOAT32 => "0.0f"
+      case TypeEnum.FLOAT64 => "0.0"
+      case TypeEnum.STRING => "\"\""
+      case TypeEnum.MAP => s"new ${getTypeName(typ, isGenericArg)}()"
+      case TypeEnum.LIST => s"new ${getTypeName(typ, isGenericArg)}()"
+      case TypeEnum.ARRAY =>
+        val t = typ.asInstanceOf[ArrayType]
+        s"new ${getTypeName(t.elementType(), false, true)}[0]"
+      case TypeEnum.ENUM => s"${getTypeName(typ)}.UNKNOWN"
+      case TypeEnum.CUSTOM => s"new ${getTypeName(typ, isGenericArg)}()"
+      case x => throw new GenerationException(s"Don't know how to handle type $x")
+    }
 
   }
 
@@ -50,24 +45,22 @@ object JavaConstruction {
     typ: Type,
     isGenericArg: Boolean = false)(implicit currentModule: Module): String = {
 
-    defNullCtroCache.getOrElseUpdate((typ, isGenericArg), {
-      typ.typeEnum() match {
-        case TypeEnum.BOOL => "false"
-        case TypeEnum.INT8 => "(byte)0"
-        case TypeEnum.INT16 => "(short)0"
-        case TypeEnum.INT32 => "0"
-        case TypeEnum.INT64 => "0L"
-        case TypeEnum.FLOAT32 => "0.0f"
-        case TypeEnum.FLOAT64 => "0.0"
-        case TypeEnum.STRING => "null"
-        case TypeEnum.MAP => "null"
-        case TypeEnum.LIST => "null"
-        case TypeEnum.ARRAY => "null"
-        case TypeEnum.CUSTOM => "null"
-        case TypeEnum.ENUM => "null"
-        case x => throw new GenerationException(s"Don't know how to handle type $x")
-      }
-    })
+    typ.typeEnum() match {
+      case TypeEnum.BOOL => "false"
+      case TypeEnum.INT8 => "(byte)0"
+      case TypeEnum.INT16 => "(short)0"
+      case TypeEnum.INT32 => "0"
+      case TypeEnum.INT64 => "0L"
+      case TypeEnum.FLOAT32 => "0.0f"
+      case TypeEnum.FLOAT64 => "0.0"
+      case TypeEnum.STRING => "null"
+      case TypeEnum.MAP => "null"
+      case TypeEnum.LIST => "null"
+      case TypeEnum.ARRAY => "null"
+      case TypeEnum.CUSTOM => "null"
+      case TypeEnum.ENUM => "null"
+      case x => throw new GenerationException(s"Don't know how to handle type $x")
+    }
 
   }
 
@@ -76,5 +69,4 @@ object JavaConstruction {
     defaultConstructNull(field.typ())
   }
 
-  
 }
