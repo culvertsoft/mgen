0b9d106918fc77a7629693551f1946c0367116b8
Johan Kjölhede <kjolhede@telia.com>
2014-07-17 09:52:00 +0100

    Added java test framework for mgen-testresources

--- mgen-javagenerator/src/main/scala/se/culvertsoft/mgen/javapack/generator/JavaToString.scala
+++ mgen-javagenerator/src/main/scala/se/culvertsoft/mgen/javapack/generator/JavaToString.scala
@@ -3,6 +3,7 @@ package se.culvertsoft.mgen.javapack.generator
 import se.culvertsoft.mgen.api.exceptions.GenerationException
 import se.culvertsoft.mgen.api.model.Type
 import se.culvertsoft.mgen.api.model.TypeEnum
+import se.culvertsoft.mgen.api.model.ArrayType
 
 object JavaToString {
 
@@ -19,7 +20,11 @@ object JavaToString {
         case TypeEnum.STRING => membName
         case TypeEnum.MAP => membName
         case TypeEnum.LIST => membName
-        case TypeEnum.ARRAY => s"java.util.Arrays.deepToString($membName)"
+        case TypeEnum.ARRAY =>
+          if (JavaGenerator.canBeNull(t.asInstanceOf[ArrayType].elementType))
+            s"java.util.Arrays.deepToString($membName)"
+          else
+            s"java.util.Arrays.toString($membName)"
         case TypeEnum.ENUM => membName
         case TypeEnum.CUSTOM => membName
         case x => throw new GenerationException(s"Don't know how to handle type $x")
