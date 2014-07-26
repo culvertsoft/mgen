ee4fff95cea43155a8399071dcd67e5a85b4c173
Johan Kjölhede <kjolhede@telia.com>
2014-07-26 15:37:54 +0100

    Improved mgen compiler speed

--- mgen-cppgenerator/src/main/scala/se/culvertsoft/mgen/cpppack/generator/impl/classh/MkEnumCpp.scala
+++ mgen-cppgenerator/src/main/scala/se/culvertsoft/mgen/cpppack/generator/impl/classh/MkEnumCpp.scala
@@ -17,7 +17,7 @@ object MkEnumCpp {
 
   def apply(module: Module, _e: EnumType, generatorSettings: java.util.Map[String, String]): String = {
 
-    implicit val txtBuffer = new SuperStringBuffer
+    implicit val txtBuffer = SuperStringBuffer.getCached()
     implicit val currentModule = module
     val namespaces = currentModule.path().split("\\.")
 
