0f6ca0a6b256434b6f3820fc5c2a16c65ddde44f
Johan Kjölhede <kjolhede@telia.com>
2014-07-29 22:16:06 +0100

    Now supporting default values for java lists/arrays. Moving on to maps, then objects

--- mgen-javagenerator/src/main/scala/se/culvertsoft/mgen/javapack/generator/impl/MkDefaultValue.scala
+++ mgen-javagenerator/src/main/scala/se/culvertsoft/mgen/javapack/generator/impl/MkDefaultValue.scala
@@ -17,22 +17,39 @@ import se.culvertsoft.mgen.api.model.NumericDefaultValue
 import se.culvertsoft.mgen.api.model.ObjectDefaultValue
 import se.culvertsoft.mgen.api.model.StringDefaultValue
 import se.culvertsoft.mgen.javapack.generator.JavaConstruction
+import se.culvertsoft.mgen.api.model.ArrayType
+import se.culvertsoft.mgen.api.model.ListType
+import se.culvertsoft.mgen.javapack.generator.JavaTypeNames
+import se.culvertsoft.mgen.api.model.DefaultValue
+
+import scala.collection.JavaConversions._
 
 object MkDefaultValue {
 
   def apply(
     f: Field,
-    nonNull: Boolean,
-    isGenericArg: Boolean = false)(implicit currentModule: Module): String = {
+    nonNull: Boolean)(implicit currentModule: Module): String = {
 
     if (!f.hasDefaultValue) {
       if (nonNull)
-        return JavaConstruction.defaultConstruct(f.typ, isGenericArg)
+        return JavaConstruction.defaultConstruct(f.typ, false)
       else
-        return JavaConstruction.defaultConstructNull(f.typ, isGenericArg)
+        return JavaConstruction.defaultConstructNull(f.typ, false)
     }
 
-    f.defaultValue match {
+    val d = f.defaultValue()
+
+    apply(d, nonNull, false, true)
+
+  }
+
+  def apply(
+    d: DefaultValue,
+    nonNull: Boolean,
+    isGenericArg: Boolean = false,
+    isFirstCall: Boolean = true)(implicit currentModule: Module): String = {
+
+    d match {
       case v: EnumDefaultValue =>
         if (v.isCurrentModule)
           s"${v.expectedType.shortName}.${v.value.name}"
@@ -53,15 +70,74 @@ object MkDefaultValue {
       case v: StringDefaultValue =>
         '"' + v.value + '"'
       case v: ListOrArrayDefaultValue =>
-        throw new GenerationException(s"Not yet implemented!")
+        val values = v.values
+        v.expectedType() match {
+          case t: ArrayType =>
+            val entries = values.map(v => apply(v, true, isGenericArg, false)).mkString(", ")
+            s"new ${JavaTypeNames.getTypeName(d.expectedType, isGenericArg, false)} { $entries }"
+          case t: ListType =>
+            val entries = values.map(v => apply(v, true, true, false)).mkString(", ")
+            s"new java.util.ArrayList<${JavaTypeNames.getTypeName(t.elementType(), true, false)}>(java.util.Arrays.asList($entries))"
+        }
       case v: MapDefaultValue =>
         throw new GenerationException(s"Not yet implemented!")
       case v: ObjectDefaultValue =>
         throw new GenerationException(s"Not yet implemented!")
       case _ =>
-        throw new GenerationException(s"Don't know how to generate default value code for $f")
+        throw new GenerationException(s"Don't know how to generate default value code for $d")
     }
 
   }
 
+  /*
+ * 
+  def getTypeName(
+    typ: Type,
+    isGenericArg: Boolean = false,
+    isArrayCtor: Boolean = false)(implicit currentModule: Module): String = {
+
+    if (typ == null)
+      return JavaConstants.mgenBaseClsString
+
+    typ.typeEnum() match {
+      case TypeEnum.ENUM =>
+        val t = typ.asInstanceOf[EnumType]
+        if (t.module == currentModule) {
+          t.shortName()
+        } else {
+          t.fullName()
+        }
+      case TypeEnum.BOOL => if (isGenericArg) "Boolean" else "boolean"
+      case TypeEnum.INT8 => if (isGenericArg) "Byte" else "byte"
+      case TypeEnum.INT16 => if (isGenericArg) "Short" else "short"
+      case TypeEnum.INT32 => if (isGenericArg) "Integer" else "int"
+      case TypeEnum.INT64 => if (isGenericArg) "Long" else "long"
+      case TypeEnum.FLOAT32 => if (isGenericArg) "Float" else "float"
+      case TypeEnum.FLOAT64 => if (isGenericArg) "Double" else "double"
+      case TypeEnum.STRING => "String"
+      case TypeEnum.MAP =>
+        val t = typ.asInstanceOf[MapType]
+        s"java.util.HashMap<${getTypeName(t.keyType(), true)}, ${getTypeName(t.valueType(), true)}>"
+      case TypeEnum.LIST =>
+        val t = typ.asInstanceOf[ListType]
+        s"java.util.ArrayList<${getTypeName(t.elementType(), true)}>"
+      case TypeEnum.ARRAY =>
+        val t = typ.asInstanceOf[ArrayType]
+        if (isArrayCtor)
+          s"${getTypeName(t.elementType(), false)}[0]"
+        else
+          s"${getTypeName(t.elementType(), false)}[]"
+      case TypeEnum.CUSTOM =>
+        val t = typ.asInstanceOf[CustomType]
+        if (t.module() == currentModule) {
+          t.name()
+        } else {
+          t.fullName()
+        }
+      case TypeEnum.UNKNOWN =>
+        throw new GenerationException("Cannot call getTypeName on an UnlinkedCustomType: " + typ.fullName)
+    }
+
+  }
+ */
 }
