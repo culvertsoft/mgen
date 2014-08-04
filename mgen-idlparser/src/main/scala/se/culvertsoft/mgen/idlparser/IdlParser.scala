04826fb879eac558e51c805c6f339b4167df47fa
Johan Kjölhede <kjolhede@telia.com>
2014-08-04 22:35:29 +0200

    cleanup

--- mgen-idlparser/src/main/scala/se/culvertsoft/mgen/idlparser/IdlParser.scala
+++ mgen-idlparser/src/main/scala/se/culvertsoft/mgen/idlparser/IdlParser.scala
@@ -8,7 +8,7 @@ import scala.collection.JavaConversions.seqAsJavaList
 
 import se.culvertsoft.mgen.api.model.ParsedSources
 import se.culvertsoft.mgen.api.model.Project
-import se.culvertsoft.mgen.api.model.impl.ProjectImpl
+import se.culvertsoft.mgen.api.model.impl.ParsedSourcesImpl
 import se.culvertsoft.mgen.api.plugins.Parser
 
 class IdlParser extends Parser {
@@ -17,7 +17,7 @@ class IdlParser extends Parser {
     sources: java.util.List[File],
     settings: java.util.Map[String, String],
     parent: Project): ParsedSources = {
-    val out = new ProjectImpl("Module_List", null, null, parent)
+    val out = new ParsedSourcesImpl()
     out.setModules(sources.map(source => ParseModule(source, settings.toMap, parent)))
     out
   }
