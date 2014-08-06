da36076078692c35dbf84cbb838fec9ec8e429da
Johan Kjölhede <kjolhede@telia.com>
2014-08-06 22:01:52 +0200

    Added stubs for XmlSchemaParser:classes and enums

---
+++ mgen-xmlschemaparser/src/main/scala/se/culvertsoft/mgen/xmlschemaparser/RichJDefinedClass.scala
@@ -0,0 +1,60 @@
+package se.culvertsoft.mgen.xmlschemaparser
+
+import com.sun.codemodel.ClassType
+import com.sun.codemodel.JClass
+import com.sun.codemodel.JClassContainer
+import com.sun.codemodel.JDefinedClass
+import com.sun.codemodel.JEnumConstant
+import com.sun.codemodel.JFieldVar
+import com.sun.codemodel.JMods
+
+object RichJDefinedClass {
+
+  private val cls = classOf[JDefinedClass]
+
+  implicit class rdclass(base: JDefinedClass) {
+
+    private def get[T](fieldName: String): T = {
+      val nameField = cls.getDeclaredField("name")
+      nameField.setAccessible(true)
+      nameField.get(base).asInstanceOf[T]
+    }
+
+    def getName() = {
+      get[String]("name")
+    }
+
+    def getSuper() = {
+      get[JClass]("superClass")
+    }
+
+    def getModifiers() = {
+      get[JMods]("mods")
+    }
+
+    def getFields() = {
+      get[java.util.Map[String, JFieldVar]]("fields")
+    }
+
+    def getParent(): JClassContainer = {
+      get[JClassContainer]("outer")
+    }
+
+    def getEnumConstants() = {
+      get[java.util.Map[String, JEnumConstant]]("enumConstantsByName")
+    }
+
+    def getInnerClasses() = {
+      get[JDefinedClass]("classes")
+    }
+
+    def isClass() = {
+      base.getClassType() == ClassType.CLASS
+    }
+
+    def isEnum() = {
+      base.getClassType() == ClassType.ENUM
+    }
+
+  }
+}
--- mgen-xmlschemaparser/src/main/scala/se/culvertsoft/mgen/xmlschemaparser/XmlSchemaParser.scala
+++ mgen-xmlschemaparser/src/main/scala/se/culvertsoft/mgen/xmlschemaparser/XmlSchemaParser.scala
@@ -4,13 +4,21 @@ import java.io.File
 import java.io.FileInputStream
 
 import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.JavaConversions.asScalaIterator
 
 import org.xml.sax.InputSource
 
+import com.sun.codemodel.JDefinedClass
+import com.sun.codemodel.JPackage
 import com.sun.tools.xjc.api.XJC
 
+import se.culvertsoft.mgen.api.exceptions.AnalysisException
+import se.culvertsoft.mgen.api.model.ClassType
+import se.culvertsoft.mgen.api.model.EnumType
+import se.culvertsoft.mgen.api.model.Module
 import se.culvertsoft.mgen.api.model.Project
 import se.culvertsoft.mgen.api.plugins.Parser
+import se.culvertsoft.mgen.xmlschemaparser.RichJDefinedClass.rdclass
 
 class XmlSchemaParser extends Parser {
 
@@ -37,8 +45,37 @@ class XmlSchemaParser extends Parser {
       compiler.parseSchema(is)
       val intermediateModel = compiler.bind()
       val jCodeModel = intermediateModel.generateCode(null, null);
+
+      for (pkg <- jCodeModel.packages) {
+
+        val moduleName = pkg.name
+
+        if (moduleName == null)
+          throw new AnalysisException(s"Null module/package name provided to $this. Need to specify a package name or use the 'replace_module_path' ")
+
+        val dir = new File(parent.absoluteFilePath).getParent
+        val fileName = pkg.name() + ".xml"
+        val module = parent.getOrCreateModule(pkg.name(), fileName, dir + "/" + fileName, settings);
+
+        for (typ <- pkg.classes) {
+          if (typ.isClass) {
+            module.addClass(cvtClass(typ, module, pkg))
+          } else if (typ.isEnum) {
+            module.addEnum(cvtEnum(typ, module, pkg))
+          }
+        }
+      }
+
     }
 
+  }
+
+  def cvtClass(typ: JDefinedClass, module: Module, pkg: JPackage): ClassType = {
+    ???
+  }
+
+  def cvtEnum(typ: JDefinedClass, module: Module, pkg: JPackage): EnumType = {
+    ???
   }
 
 }
