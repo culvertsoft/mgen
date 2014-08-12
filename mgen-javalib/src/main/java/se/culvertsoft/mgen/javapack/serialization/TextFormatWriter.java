b71b5f9fec10d255650469dedc6900d448117ee6
Johan Kjölhede <kjolhede@telia.com>
2014-08-13 00:07:47 +0200

    opened up TextFormatWriter for overloads

--- mgen-javalib/src/main/java/se/culvertsoft/mgen/javapack/serialization/TextFormatWriter.java
+++ mgen-javalib/src/main/java/se/culvertsoft/mgen/javapack/serialization/TextFormatWriter.java
@@ -10,12 +10,12 @@ public abstract class TextFormatWriter extends BuiltInWriter {
 	public static int FLUSH_SIZE = STRING_ENCODE_BUFFER_SIZE / 4;
 
 	private OutputStream m_stream;
-	private final StringBuilder m_buffer;
+	protected final StringBuilder m_textBuffer;
 
 	public TextFormatWriter(final OutputStream stream, final ClassRegistryBase classRegistry) {
 		super(classRegistry);
 		m_stream = stream;
-		m_buffer = new StringBuilder(FLUSH_SIZE * 2);
+		m_textBuffer = new StringBuilder(FLUSH_SIZE * 2);
 	}
 	
 	protected TextFormatWriter setOutput(final OutputStream stream) {
@@ -24,42 +24,42 @@ public abstract class TextFormatWriter extends BuiltInWriter {
 	}
 
 	protected void write(final boolean b) throws IOException {
-		m_buffer.append(b);
+		m_textBuffer.append(b);
 		checkflush();
 	}
 
 	protected void write(final byte b) throws IOException {
-		m_buffer.append(b);
+		m_textBuffer.append(b);
 		checkflush();
 	}
 
 	protected void write(final char s) throws IOException {
-		m_buffer.append(s);
+		m_textBuffer.append(s);
 		checkflush();
 	}
 
 	protected void write(final short s) throws IOException {
-		m_buffer.append(s);
+		m_textBuffer.append(s);
 		checkflush();
 	}
 
 	protected void write(final int i) throws IOException {
-		m_buffer.append(i);
+		m_textBuffer.append(i);
 		checkflush();
 	}
 
 	protected void write(final long i) throws IOException {
-		m_buffer.append(i);
+		m_textBuffer.append(i);
 		checkflush();
 	}
 
 	protected void write(final float f) throws IOException {
-		m_buffer.append(f);
+		m_textBuffer.append(f);
 		checkflush();
 	}
 
 	protected void write(final double d) throws IOException {
-		m_buffer.append(d);
+		m_textBuffer.append(d);
 		checkflush();
 	}
 
@@ -68,16 +68,26 @@ public abstract class TextFormatWriter extends BuiltInWriter {
 			write(s.charAt(i));
 	}
 
+	/**
+	 * Marked protected to allow writing to non-streams, such as string buffers
+	 * 
+	 * @throws IOException
+	 */
 	protected void flush() throws IOException {
-		if (m_buffer.length() > 0) {
-			m_stringEncoder.encode(m_buffer);
+		if (m_textBuffer.length() > 0) {
+			m_stringEncoder.encode(m_textBuffer);
 			m_stream.write(m_stringEncoder.data(), 0, m_stringEncoder.size());
-			m_buffer.setLength(0);
+			m_textBuffer.setLength(0);
 		}
 	}
 
-	private void checkflush() throws IOException {
-		if (m_buffer.length() >= FLUSH_SIZE)
+	/**
+	 * Marked protected to allow custom flush conditions
+	 * 
+	 * @throws IOException
+	 */
+	protected void checkflush() throws IOException {
+		if (m_textBuffer.length() >= FLUSH_SIZE)
 			flush();
 	}
 
