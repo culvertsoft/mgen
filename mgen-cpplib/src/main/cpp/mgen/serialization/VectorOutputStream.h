14b00dca248c7a2b8c8303f7b980b6ff700e1bbe
Johan Kjölhede <kjolhede@telia.com>
2014-08-28 21:43:05 +0200

    improving c++ documentation and interfaces

--- mgen-cpplib/src/main/cpp/mgen/serialization/VectorOutputStream.h
+++ mgen-cpplib/src/main/cpp/mgen/serialization/VectorOutputStream.h
@@ -13,13 +13,27 @@
 
 namespace mgen {
 
+/**
+ * Represents an in-memory data output for MGen writers. VectorOutputStream wraps
+ * a provided std::vector<char> without making a copy. When data is written to this 
+ * VectorOutputStream, the bytes are pushed back to the underlying wrapped vector.
+ */
 class VectorOutputStream {
 public:
 
+    /**
+     * Wraps a provided vector in this VectorOutputStream. No copy is made,
+     * so the caller is responsible for keeping the wrapped vector alive for as long
+     * as this VectorOutputStream exists.
+     */
     VectorOutputStream(std::vector<char>& data) :
             m_data(data) {
     }
 
+    /**
+     * Writes nBytes bytes to the underlying wrapped vector. The bytes are pushed back
+     * at the end of the underlying wrapped vector.
+     */
     void write(const void* src, const int nBytes) {
         m_data.insert(
                 m_data.end(),
@@ -27,6 +41,9 @@ public:
                 reinterpret_cast<const char*>(src) + nBytes);
     }
 
+    /**
+     * Clears the underlying wrapped vector (std::vector<char>::clear()).
+     */
     void reset() {
         m_data.clear();
     }
