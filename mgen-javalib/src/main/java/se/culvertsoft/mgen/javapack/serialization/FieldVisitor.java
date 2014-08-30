7a747040d10e8902c00a1f55ae6354e8ccead06a
Johan Kjölhede <kjolhede@telia.com>
2014-08-30 09:13:23 +0200

    Adding more java documentation

--- mgen-javalib/src/main/java/se/culvertsoft/mgen/javapack/serialization/FieldVisitor.java
+++ mgen-javalib/src/main/java/se/culvertsoft/mgen/javapack/serialization/FieldVisitor.java
@@ -5,40 +5,204 @@ import java.io.IOException;
 import se.culvertsoft.mgen.api.model.Field;
 import se.culvertsoft.mgen.javapack.classes.MGenBase;
 
+/**
+ * Base interface of all MGen object visitors. It is used to visit MGen objects
+ * and their fields. All generated MGen classes have _accept(..) methods which
+ * takes FieldVisitor instances as arguments. The object then calls back this
+ * visitor for each field selected for visitation.
+ */
 public interface FieldVisitor {
 
+	/**
+	 * Called when starting to visit an MGen object and its fields.
+	 * 
+	 * @param object
+	 *            The object that is being visited
+	 * 
+	 * @param nFields
+	 *            The number of fields that will be visited
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
 	public void beginVisit(final MGenBase object, final int nFields) throws IOException;
 
-	public void visit(final boolean b, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit a boolean field
+	 * 
+	 * @param b
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final boolean b, final Field field) throws IOException;
 
-	public void visit(final byte b, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit an int8 field
+	 * 
+	 * @param b
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final byte b, final Field field) throws IOException;
 
-	public void visit(final short s, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit an int16 field
+	 * 
+	 * @param s
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final short s, final Field field) throws IOException;
 
-	public void visit(final int i, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit an int32 field
+	 * 
+	 * @param i
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final int i, final Field field) throws IOException;
 
-	public void visit(final long l, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit an int64 field
+	 * 
+	 * @param l
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final long l, final Field field) throws IOException;
 
-	public void visit(final float f, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit a float32 field
+	 * 
+	 * @param f
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final float f, final Field field) throws IOException;
 
-	public void visit(final double d, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit a float64 field
+	 * 
+	 * @param d
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final double d, final Field field) throws IOException;
 
-	public void visit(final String s, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit a string field
+	 * 
+	 * @param s
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final String s, final Field field) throws IOException;
 
-	public void visit(final Enum<?> e, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit an enum field
+	 * 
+	 * @param e
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final Enum<?> e, final Field field) throws IOException;
 
-	public void visit(final Object o, final Field field)
-			throws IOException;
+	/**
+	 * Called to visit a list, array, map or MGen object field
+	 * 
+	 * @param o
+	 *            The field value
+	 * 
+	 * @param field
+	 *            The field type metadata
+	 * 
+	 * @throws IOException
+	 *             FieldVisitor instances are usually serializers (writers), and
+	 *             thus write to an underlying data output stream. This
+	 *             exception declaration is to pass on any IOExceptions thrown
+	 *             when writing to that underlying data output stream.
+	 */
+	public void visit(final Object o, final Field field) throws IOException;
 
+	/**
+	 * Called after all the fields of an object have been visited.
+	 */
 	public void endVisit() throws IOException;
 
 }
--- mgen-javalib/src/main/java/se/culvertsoft/mgen/javapack/serialization/JsonPrettyWriter.java
+++ mgen-javalib/src/main/java/se/culvertsoft/mgen/javapack/serialization/JsonPrettyWriter.java
@@ -5,8 +5,37 @@ import java.io.OutputStream;
 
 import se.culvertsoft.mgen.javapack.classes.ClassRegistryBase;
 
+/**
+ * An MGen wire format writer for writing objects to data streams in JSON format.
+ * This class does the same as JsonWriter, except that it also adds newlines and
+ * indentation to the written JSON so it is more human readable.
+ */
 public class JsonPrettyWriter extends JsonWriter {
 
+	/**
+	 * Creates a new JsonPrettyWriter
+	 * 
+	 * @param outputStream
+	 *            The data output stream to write to
+	 * 
+	 * @param classRegistry
+	 *            The class registry to use
+	 * 
+	 * @param compact
+	 *            If to use standard of compact mode. In compact mode, MGen
+	 *            object's type metadata is only written to the stream when it
+	 *            cannot be inferred by the Reader.
+	 * 
+	 * @param maxDepth
+	 *            The maximum recursion depth allowed for writing objects. While
+	 *            writing the contents of arrays, maps, objects, lists etc the
+	 *            current recursion depth is increased by one. The default value
+	 *            is DEFAULT_MAX_DEPTH.
+	 * 
+	 * @param includeTransientFields
+	 *            If fields flagged as transient should also be written. The
+	 *            default is not to write transient fields.
+	 */
 	public JsonPrettyWriter(
 			final OutputStream outputStream,
 			final ClassRegistryBase classRegistry,
@@ -15,7 +44,28 @@ public class JsonPrettyWriter extends JsonWriter {
 			final boolean includeTransientFields) {
 		super(outputStream, classRegistry, compact, maxDepth, includeTransientFields);
 	}
-	
+
+	/**
+	 * Creates a new JsonPrettyWriter
+	 * 
+	 * @param outputStream
+	 *            The data output stream to write to
+	 * 
+	 * @param classRegistry
+	 *            The class registry to use
+	 * 
+	 * @param compact
+	 *            If to use standard of compact mode. In compact mode, MGen
+	 *            object's type metadata is only written to the stream when it
+	 *            cannot be inferred by the Reader. The default setting is
+	 *            standard mode.
+	 * 
+	 * @param maxDepth
+	 *            The maximum recursion depth allowed for writing objects. While
+	 *            writing the contents of arrays, maps, objects, lists etc the
+	 *            current recursion depth is increased by one. The default value
+	 *            is DEFAULT_MAX_DEPTH.
+	 */
 	public JsonPrettyWriter(
 			final OutputStream outputStream,
 			final ClassRegistryBase classRegistry,
@@ -24,6 +74,21 @@ public class JsonPrettyWriter extends JsonWriter {
 		super(outputStream, classRegistry, compact, maxDepth);
 	}
 
+	/**
+	 * Creates a new JsonPrettyWriter
+	 * 
+	 * @param outputStream
+	 *            The data output stream to write to
+	 * 
+	 * @param classRegistry
+	 *            The class registry to use
+	 * 
+	 * @param compact
+	 *            If to use standard of compact mode. In compact mode, MGen
+	 *            object's type metadata is only written to the stream when it
+	 *            cannot be inferred by the Reader. The default setting is
+	 *            standard mode.
+	 */
 	public JsonPrettyWriter(
 			final OutputStream outputStream,
 			final ClassRegistryBase classRegistry,
@@ -31,18 +96,42 @@ public class JsonPrettyWriter extends JsonWriter {
 		this(outputStream, classRegistry, compact, DEFAULT_MAX_DEPTH);
 	}
 
-	public JsonPrettyWriter(
-			final OutputStream outputStream,
-			final ClassRegistryBase classRegistry) {
+	/**
+	 * Creates a new JsonPrettyWriter
+	 * 
+	 * @param outputStream
+	 *            The data output stream to write to
+	 * 
+	 * @param classRegistry
+	 *            The class registry to use
+	 */
+	public JsonPrettyWriter(final OutputStream outputStream, final ClassRegistryBase classRegistry) {
 		this(outputStream, classRegistry, DEFAULT_COMPACT);
 	}
 
+	/**
+	 * Internal overloaded method to produce pretty JSON.
+	 * 
+	 * @param name
+	 *            The name of the field/map key
+	 * 
+	 * @throws IOException
+	 *             If an IOException occurs when writing to the the underlying
+	 *             output stream
+	 */
 	@Override
 	protected void writeName(final String name) throws IOException {
 		super.writeName(name);
 		write(' ');
 	}
 
+	/**
+	 * Internal overloaded method to produce pretty JSON.
+	 * 
+	 * @throws IOException
+	 *             If an IOException occurs when writing to the the underlying
+	 *             output stream
+	 */
 	@Override
 	protected void newEntry() throws IOException {
 		super.newEntry();
@@ -51,9 +140,22 @@ public class JsonPrettyWriter extends JsonWriter {
 			write('\t');
 	}
 
+	/**
+	 * Internal overloaded method to produce pretty JSON.
+	 * 
+	 * @param endString
+	 *            The string that finishes this block (e.g. a ']' for the end of
+	 *            writing an a array)
+	 * 
+	 * @param hasContents
+	 *            If the block was non-empty
+	 * 
+	 * @throws IOException
+	 *             If an IOException occurs when writing to the the underlying
+	 *             output stream
+	 */
 	@Override
-	protected void endBlock(final String endString, final boolean hasContents)
-			throws IOException {
+	protected void endBlock(final String endString, final boolean hasContents) throws IOException {
 		if (hasContents) {
 			write('\n');
 			for (int i = 0; i < m_depth - 1; i++)
